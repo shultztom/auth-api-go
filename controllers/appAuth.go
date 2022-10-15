001c6eee3ea38ccc702d3e0d1eed2c760ca91c70
shultztom <shultzthomask@sau.edu>
2022-10-14 19:29:38 -0500

    added new route and fixed bug

--- controllers/appAuth.go
+++ controllers/appAuth.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"auth-api-go/models"
 	"auth-api-go/utils"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt"
@@ -16,13 +17,39 @@ type AppClaims struct {
 // AppVerify GET /app/verify
 func AppVerify(c *gin.Context) {
 	appJwtKey := []byte(os.Getenv("JWT_APP_SECRET"))
-	token := utils.ParseToken(c, appJwtKey, "X-API-Token")
+	token, err := utils.ParseToken(c, appJwtKey, "X-API-Token")
 
 	isValid := token.Valid
 
-	if isValid {
+	if isValid && err != nil {
 		c.JSON(http.StatusOK, gin.H{"message": "success"})
 	} else {
 		c.JSON(http.StatusForbidden, gin.H{"error": "Invalid Token!"})
 	}
+}
+
+// AppDeleteUser Delete /app/user
+func AppDeleteUser(c *gin.Context) {
+	appJwtKey := []byte(os.Getenv("JWT_APP_SECRET"))
+	token, err := utils.ParseToken(c, appJwtKey, "X-API-Token")
+	if err != nil {
+		return
+	}
+
+	if !token.Valid {
+		return
+	}
+
+	// Get user from request
+	username := c.Param("username")
+
+	var user models.User
+
+	result := models.DB.Where("username = ?", username).Delete(&user)
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": result.Error})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"Deleted user": username})
 }
