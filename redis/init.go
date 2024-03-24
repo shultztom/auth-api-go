ec32e2c7254ca5076ffa2d94bfc9dd337b503f11
shultztom <shultzthomask@sau.edu>
2024-03-24 13:19:58 -0500

    moved redis url to REDIS_URL env var

--- redis/init.go
+++ redis/init.go
@@ -3,7 +3,10 @@ package redis
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/redis/go-redis/v9"
 )
 
@@ -12,8 +15,23 @@ var REDIS *redis.Client
 func ConnectRedis() {
 	ctx := context.Background()
 
+	// Load env vars
+	err := godotenv.Load()
+
+	isCloud := os.Getenv("IS_CLOUD")
+
+	if err != nil {
+		// Ignore error if running in k8s
+		if isCloud != "true" {
+			log.Fatal("Error loading .env file")
+			return
+		}
+	}
+
+	redisUrl := os.Getenv("REDIS_URL")
+
 	client := redis.NewClient(&redis.Options{
-		Addr:     "10.0.0.98:6379",
+		Addr:     redisUrl,
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
